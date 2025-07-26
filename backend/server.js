ae6fd1e6452b4ce942d93a1f725b2af16407492c
Yusuf UYAR <uyaryusuf01@gmail.com>
2025-07-26 11:13:43 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 // CORS ayarları
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://playwright-frontend.onrender.com', 'https://otomasyon-arayuz.onrender.com']
+    ? ['https://otomasyon-arayuz.vercel.app', 'https://otomasyon-arayuz.onrender.com']
     : ['http://localhost:3000', 'http://192.168.56.1:3000'],
   credentials: true
 }));
